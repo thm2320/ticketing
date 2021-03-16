7825fc3823607fc5aa628a87d276c11fc0880126
thm2320 <70251202+thm2320@users.noreply.github.com>
2021-03-16 16:22:30 +0800

    handle page not found

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,6 +6,7 @@ import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -14,6 +15,11 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+//all will make it available to all http methods
+app.all("*", () => {
+  throw new NotFoundError();
+});
 
 //express will determine if it is error handler by the number of parameters
 app.use(errorHandler);
