e6b2f0a638131044c551b34ba862f620371d5081
thm2320 <70251202+thm2320@users.noreply.github.com>
2021-03-16 20:31:43 +0800

    saving user and also check existing

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { User } from "../models/user";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
@@ -18,18 +19,22 @@ router.post(
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      //This is javascript (not ts)
-      /*
-      const error = new Error("Invalid email or password");
-      error.reasons = errors.array();
-      */
       throw new RequestValidationError(errors.array());
     }
 
-    console.log("Creating a user...");
-    throw new DatabaseConnectionError();
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
 
-    res.send({});
+    if (existingUser) {
+      // console.log("Email in use");
+      // return res.send({});
+      throw new BadRequestError("Email in use");
+    }
+
+    const user = new User({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
