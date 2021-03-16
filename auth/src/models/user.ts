10add74c05e4a006f269ab83b1b62eda8286a784
thm2320 <70251202+thm2320@users.noreply.github.com>
2021-03-16 18:39:36 +0800

    better solution makes mongoose work with Typescript

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,8 +1,57 @@
+//better solution
+import mongoose from "mongoose";
+
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+type UserDoc = mongoose.Document & UserAttrs;
+
+const userSchema = new mongoose.Schema<UserDoc>({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const UserModel = mongoose.model<UserDoc>("User", userSchema);
+
+export class User extends UserModel {
+  constructor(attrs: UserAttrs) {
+    super(attrs);
+  }
+}
+
+const user = new User({
+  email: "test@test.com",
+  password: "password"
+});
+
+//old solution
+/* 
 import mongoose from "mongoose";
 
 // An interface that describes the properties
 // that are required to create a new User
 interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// An interface that describes the properties
+// that a User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties
+// that a User Document has
+interface UserDoc extends mongoose.Document {
   email: string;
   password: string;
 }
@@ -17,11 +66,11 @@ const userSchema = new mongoose.Schema({
     required: true
   }
 });
-
-const User = mongoose.model("User", userSchema);
-
-const buildUser = (attrs: UserAttrs) => {
+userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
 
-export { User, buildUser };
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+export { User };
+ */
