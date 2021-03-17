38124e5bfc7439b9591a4ff3f188c685bf7378cf
thm2320 <70251202+thm2320@users.noreply.github.com>
2021-03-17 11:00:30 +0800

    manually implement the hash process and add to user pre save hook for password

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,6 @@
 //better solution
 import mongoose from "mongoose";
+import { Password } from "../services/password";
 
 interface UserAttrs {
   email: string;
@@ -17,6 +18,15 @@ const userSchema = new mongoose.Schema<UserDoc>({
     type: String,
     required: true
   }
+});
+
+userSchema.pre("save", async function (done) {
+  console.log(this);
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+  done();
 });
 
 const UserModel = mongoose.model<UserDoc>("User", userSchema);
