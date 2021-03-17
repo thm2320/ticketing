11f8c12585afabb05161cb565acd5edbd5aa92f2
thm2320 <70251202+thm2320@users.noreply.github.com>
2021-03-17 14:57:49 +0800

    format the JSON property of user using model toJSON

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -9,16 +9,28 @@ interface UserAttrs {
 
 type UserDoc = mongoose.Document & UserAttrs;
 
-const userSchema = new mongoose.Schema<UserDoc>({
-  email: {
-    type: String,
-    required: true
+const userSchema = new mongoose.Schema<UserDoc>(
+  {
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
   },
-  password: {
-    type: String,
-    required: true
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      }
+    }
   }
-});
+);
 
 userSchema.pre("save", async function (done) {
   console.log(this);
