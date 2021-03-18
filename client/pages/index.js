702972234846a34c1dec1f35ad242521fa67f851
thm2320 <70251202+thm2320@users.noreply.github.com>
2021-03-18 17:47:23 +0800

    requesting auth service from ingress-ngnix of another name-space

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,34 @@
-const index = () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  /* useEffect(() => {
+    axios.get("/api/users/currentuser");
+  }, []); */
+  console.log(currentUser);
+
   return <h1>Landing Page</h1>;
 };
-export default index;
+
+LandingPage.getInitialProps = async ({ req }) => {
+  console.log(req.headers);
+  if (typeof window === "undefined") {
+    //we are on the server
+    //requests should be made to http://{ingress-service}.{namespace}.xxxx
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: req.headers
+      }
+    );
+    return data;
+  } else {
+    //we are on the browser
+    //requests should be made with a base url of ''
+    const { data } = await axios.get("/api/users/currentuser");
+    return data;
+  }
+
+  return {};
+};
+
+export default LandingPage;
