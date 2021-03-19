152c62de88ed02fe71f1d939c20690e81b026efd
thm2320 <70251202+thm2320@users.noreply.github.com>
2021-03-19 11:44:24 +0800

    Test Driven Development (TDD) for ticketService

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,8 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
-import { errorHandler, NotFoundError } from "@thmtickets/common";
+import { errorHandler, NotFoundError, currentUser } from "@thmtickets/common";
+import { createTicketRouter } from "./routes/new";
 
 const app = express();
 app.set("trust proxy", true); //tell express to trust the proxy from ingress-nginx
@@ -13,6 +14,10 @@ app.use(
     secure: process.env.NODE_ENV !== "test"
   })
 );
+
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 //all will make it available to all http methods
 app.all("*", async (req, res, next) => {
