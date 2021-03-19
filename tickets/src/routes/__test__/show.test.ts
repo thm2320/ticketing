d9a16dc515805755f4b10df0b4770e84d106ff32
thm2320 <70251202+thm2320@users.noreply.github.com>
2021-03-19 15:20:31 +0800

    update the missing test in showTicket

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -4,9 +4,10 @@ import mongoose from "mongoose";
 
 it("returns a 404 if the ticket is not found", async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
-  const response = await request(app).get(`/api/tickets/${id}`).send();
-
-  console.log(response.body);
+  const response = await request(app)
+    .get(`/api/tickets/${id}`)
+    .send()
+    .expect(404);
 });
 
 it("returns the ticket if the ticket is found", async () => {
