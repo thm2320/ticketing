3138966dd7058033e99d3d0350e9479e65692299
thm2320 <70251202+thm2320@users.noreply.github.com>
2021-03-19 12:32:08 +0800

    create ticket model

---
+++ tickets/src/models/ticket.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+interface TicketAttrs {
+  title: string;
+  price: number;
+  userId: string;
+}
+
+type TicketDoc = mongoose.Document & TicketAttrs;
+
+const ticketSchema = new mongoose.Schema<TicketDoc>(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    userId: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      }
+    }
+  }
+);
+
+const TicketModel = mongoose.model<TicketDoc>("Ticket", ticketSchema);
+
+export class Ticket extends TicketModel {
+  constructor(attrs: TicketAttrs) {
+    super(attrs);
+  }
+}
