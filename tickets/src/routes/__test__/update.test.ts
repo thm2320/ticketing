ec5f071fffacfd3f12f3abaffa12e4f3c721f00a
thm2320 <70251202+thm2320@users.noreply.github.com>
2021-03-23 15:17:55 +0800

    test event sent by mocking the client publish method

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import { app } from "../../app";
 import mongoose from "mongoose";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("returns a 404 if the provided id does not exist", async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
@@ -100,4 +101,27 @@ it("updates the ticket provided valid title and price", async () => {
 
   expect(ticketResposne.body.title).toEqual("new title");
   expect(ticketResposne.body.price).toEqual(100);
+});
+
+it("publishes an event", async () => {
+  const cookie = global.signin();
+
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      title: "asdasdasd",
+      price: 20
+    });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({
+      title: "new title",
+      price: 100
+    })
+    .expect(200);
+
+  expect(natsWrapper.client.publish).toBeCalledTimes(2);
 });
