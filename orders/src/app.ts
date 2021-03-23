3b526b5ec6c155fd60ba580d8de03004143dcfcd
thm2320 <70251202+thm2320@users.noreply.github.com>
2021-03-23 16:16:14 +0800

    starting code of orderService (copy from ticketService and modify)

---
+++ orders/src/app.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError, currentUser } from "@thmtickets/common";
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes/index";
+import { udpateTicketRouter } from "./routes/update";
+
+const app = express();
+app.set("trust proxy", true); //tell express to trust the proxy from ingress-nginx
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test"
+  })
+);
+
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(udpateTicketRouter);
+
+//all will make it available to all http methods
+app.all("*", async (req, res, next) => {
+  throw new NotFoundError();
+});
+
+//express will determine if it is error handler by the number of parameters
+app.use(errorHandler);
+
+export { app };
