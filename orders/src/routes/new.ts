61ca80c0d5e4da399b4f2c61664a3cc2abd576cc
thm2320 <70251202+thm2320@users.noreply.github.com>
2021-03-23 22:04:15 +0800

    implement the "new" route for orders

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,46 @@
+import mongoose from "mongoose";
 import express, { Request, Response } from "express";
+import {
+  NotFoundError,
+  requireAuth,
+  validateRequest
+} from "@thmtickets/common";
+import { body } from "express-validator";
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("Valid TicketId must be provided")
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    // Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // Make sure that this ticket is not reserved
+
+    // Calculate the expiration date for this order
+
+    // Build the order and save it to the database
+
+    // Publish an event saying that an order was created
+
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
