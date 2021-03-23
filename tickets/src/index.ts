5c933766fd354eb387595527373fa0af259dfe08
thm2320 <70251202+thm2320@users.noreply.github.com>
2021-03-23 12:51:32 +0800

    handle nats shutdown

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -12,6 +12,20 @@ const start = async () => {
 
   try {
     await natsWrapper.connect("ticketing", "asdsadas", "http://nats-srv:4222");
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+    process.on("SIGINT", () => {
+      console.log("SIGINT");
+      natsWrapper.client.close();
+    });
+    process.on("SIGIERM", () => {
+      // not work in windows using "rs"
+      console.log("SIGIERM");
+      natsWrapper.client.close();
+    });
+
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
