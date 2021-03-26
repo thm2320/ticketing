fcf370891ab31564ba55318f5b4c24ed6c21be54
thm2320 <70251202+thm2320@users.noreply.github.com>
2021-03-26 11:32:51 +0800

    implement the order-created-listener in payments

---
+++ payments/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,24 @@
+import { Message } from "node-nats-streaming";
+import { Listener, OrderCreatedEvent, Subjects } from "@thmtickets/common";
+import { queueGroupName } from "./queue-group-name";
+import { Order } from "../../models/order";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    console.log(data);
+    console.log(data.ticket.price);
+    const order = new Order({
+      _id: data.id,
+      price: data.ticket.price,
+      status: data.status,
+      userId: data.userId,
+      version: data.version
+    });
+    await order.save();
+
+    msg.ack();
+  }
+}
