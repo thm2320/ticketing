4198eb3e9f08f5c9ad5c2c77f962363fa47bd8d4
thm2320 <70251202+thm2320@users.noreply.github.com>
2021-03-26 10:44:22 +0800

    start up code for payments

---
+++ payments/src/app.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError, currentUser } from "@thmtickets/common";
+
+const app = express();
+app.set("trust proxy", true); //tell express to trust the proxy from ingress-nginx
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test"
+  })
+);
+
+app.use(currentUser);
+
+//all will make it available to all http methods
+app.all("*", async (req, res, next) => {
+  throw new NotFoundError();
+});
+
+//express will determine if it is error handler by the number of parameters
+app.use(errorHandler);
+
+export { app };
