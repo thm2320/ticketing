28c2af9cd824bb630d56885f9885478a948857bc
thm2320 <70251202+thm2320@users.noreply.github.com>
2021-03-26 17:48:54 +0800

    Payment model

---
+++ payments/src/models/payment.ts
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+interface PaymentAttrs {
+  orderId: string;
+  stripeId: string;
+}
+
+interface PaymentDoc extends mongoose.Document {
+  orderId: string;
+  stripeId: string;
+}
+
+interface PaymentModel extends mongoose.Model<PaymentDoc> {}
+
+const paymentSchema = new mongoose.Schema(
+  {
+    orderId: {
+      required: true,
+      type: String
+    },
+    stripeId: {
+      required: true,
+      type: String
+    }
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      }
+    }
+  }
+);
+
+const PaymentCls = mongoose.model<PaymentDoc, PaymentModel>(
+  "Payment",
+  paymentSchema
+);
+
+export class Order extends PaymentCls {
+  constructor(attrs: PaymentAttrs) {
+    super(attrs);
+  }
+}
