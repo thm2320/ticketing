44c1bdccb52b65a53771c75562ed976f93df23f3
thm2320 <70251202+thm2320@users.noreply.github.com>
2021-03-26 11:33:01 +0800

    test for order-created-listener

---
+++ payments/src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,46 @@
+import { Message } from "node-nats-streaming";
+import { OrderCreatedEvent, OrderStatus } from "@thmtickets/common";
+import mongoose from "mongoose";
+import { natsWrapper } from "../../../nats-wrapper";
+import { OrderCreatedListener } from "../order-created-listener";
+import { Order } from "../../../models/order";
+
+const setup = async () => {
+  const listener = new OrderCreatedListener(natsWrapper.client);
+
+  const data: OrderCreatedEvent["data"] = {
+    id: mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    expiresAt: "adasdasd",
+    userId: "asdasda",
+    status: OrderStatus.Created,
+    ticket: {
+      id: "adasdad",
+      price: 10
+    }
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn()
+  };
+
+  return { listener, data, msg };
+};
+
+it("replacates the order info", async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+  const order = await Order.findById(data.id);
+
+  expect(order!.price).toEqual(data.ticket.price);
+});
+
+it("ack the message", async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(msg.ack).toHaveBeenCalled();
+});
