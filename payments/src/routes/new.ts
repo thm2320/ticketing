ea6c59abd30d636d1604b27bac27a5515abffcfc
thm2320 <70251202+thm2320@users.noreply.github.com>
2021-03-26 14:49:37 +0800

    implement the fail case for creating payment

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -4,7 +4,9 @@ import {
   requireAuth,
   validateRequest,
   BadRequestError,
-  NotFoundError
+  NotFoundError,
+  NotAuthorizedError,
+  OrderStatus
 } from "@thmtickets/common";
 import { Order } from "../models/order";
 
@@ -16,7 +18,20 @@ router.post(
   [body("token").not().isEmpty(), body("orderId").not().isEmpty()],
   validateRequest,
   async (req: Request, res: Response) => {
-    console.log(123);
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError("Cannot pay for an cancelled order");
+    }
+
     res.send({ success: true });
   }
 );
