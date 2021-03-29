2ba00c9c83dbaa4c1ef807c0a5e1e24c50a7bdae
thm2320 <70251202+thm2320@users.noreply.github.com>
2021-03-29 14:37:43 +0800

    implement order List

---
+++ client/pages/orders/index.js
@@ -0,0 +1,24 @@
+import buildClient from "../../api/build-client";
+
+const OrderIndex = ({ orders }) => {
+  return (
+    <ul>
+      {orders.map((order) => {
+        return (
+          <li key={order.id}>
+            {order.ticket.title} - {order.status}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export async function getServerSideProps(context) {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/orders");
+
+  return { props: { orders: data } };
+}
+
+export default OrderIndex;
