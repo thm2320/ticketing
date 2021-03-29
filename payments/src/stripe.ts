07b515f83c277039e5c202d3aa02b98e520264f7
thm2320 <70251202+thm2320@users.noreply.github.com>
2021-03-29 21:09:46 +0800

    remove console.log

--- payments/src/stripe.ts
+++ payments/src/stripe.ts
@@ -1,8 +1,5 @@
 import Stripe from "stripe";
 
-console.log("process.env.STRIPE_KEY");
-console.log(process.env.STRIPE_KEY!);
-
 export const stripe = new Stripe(process.env.STRIPE_KEY!, {
   apiVersion: "2020-08-27"
 });
